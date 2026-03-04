e2d85da884264c88a80aeb879aa5d7203a899163
eljamm <fedi.jamoussi@protonmail.ch>
2026-03-04 16:49:35 +0100

    wip: expose nixos config in eval option

--- forge/modules/apps/nixos/default.nix
+++ forge/modules/apps/nixos/default.nix
@@ -3,6 +3,7 @@
   inputs,
 
   app,
+  config,
   ...
 }:
 {
@@ -110,11 +111,31 @@
         '';
       };
 
+      eval = lib.mkOption {
+        type = lib.types.attrsOf lib.types.anything;
+        default = { };
+        description = ""; # TODO:
+      };
+
       build = lib.mkOption {
         internal = true;
         readOnly = true;
         type = lib.types.package;
-        default =
+        default = config.vm.config.system.build.vm;
+        description = ""; # TODO:
+      };
+    };
+  };
+
+  config = lib.mkIf config.enable {
+    vm.eval = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        (
+          {
+            pkgs,
+            ...
+          }:
           let
             forwardPortsAttrs =
               ports:
@@ -129,37 +150,27 @@
                   guest.port = lib.toInt (lib.elemAt portSplit 1);
                 }
               ) ports;
-
-            vm = inputs.nixpkgs.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                (
-                  { pkgs, ... }:
-                  lib.recursiveUpdate {
-                    imports = [ "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix" ];
-                    users.users.root.password = "root";
-                    services.openssh.settings.PermitRootLogin = lib.mkForce "yes";
-                    services.openssh.settings.PasswordAuthentication = lib.mkForce true;
-                    services.getty.autologinUser = "root";
-                    environment.systemPackages = app.vm.requirements;
-                    networking.hostName = app.vm.name;
-                    networking.useDHCP = lib.mkForce true;
-                    networking.firewall.enable = lib.mkForce false;
-                    virtualisation.graphics = false;
-                    virtualisation.cores = app.vm.config.cores;
-                    virtualisation.memorySize = app.vm.config.memorySize;
-                    virtualisation.diskSize = app.vm.config.diskSize;
-                    virtualisation.forwardPorts = forwardPortsAttrs app.vm.config.ports;
-                    system.stateVersion = "25.11";
-                  } app.vm.config.system
-                )
-              ];
-            };
           in
-          vm.config.system.build.vm;
-
-        description = ""; # TODO:
-      };
+          lib.recursiveUpdate {
+            imports = [ "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix" ];
+            users.users.root.password = "root";
+            services.openssh.settings.PermitRootLogin = lib.mkForce "yes";
+            services.openssh.settings.PasswordAuthentication = lib.mkForce true;
+            services.getty.autologinUser = "root";
+            environment.systemPackages = app.vm.requirements;
+            networking.hostName = app.vm.name;
+            networking.useDHCP = lib.mkForce true;
+            networking.firewall.enable = lib.mkForce false;
+            virtualisation.graphics = false;
+            virtualisation.cores = app.vm.config.cores;
+            virtualisation.memorySize = app.vm.config.memorySize;
+            virtualisation.diskSize = app.vm.config.diskSize;
+            virtualisation.forwardPorts = forwardPortsAttrs app.vm.config.ports;
+            system.stateVersion = "25.11";
+            passthru = { }; # FIX: why is this required?
+          } app.vm.config.system
+        )
+      ];
     };
   };
 }
