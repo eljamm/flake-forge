6a91629c1eb9d6bf24f153396016bfea5583604c
eljamm <fedi.jamoussi@protonmail.ch>
2026-03-04 11:07:59 +0100

    mox: add app recipe

---
+++ recipes_nimi/apps/mox-app/recipe.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  name = "mox-app";
+  version = "0.0.15";
+  description = "Modern full-featured open source secure mail server";
+  usage = ''
+    Mox is a modern mail server that aims to be fully featured and easy to use.
+
+    * Quickstart (first time setup):
+    ```
+    mox quickstart hostname user@domain
+    ```
+
+    * Serve mail:
+    ```
+    mox -config /var/lib/mox/config/mox.conf serve
+    ```
+  '';
+
+  services = {
+    mox = {
+      process.argv = [
+        (lib.getExe pkgs.mypkgs.mox)
+        "-config"
+        "/var/lib/mox/config/mox.conf"
+        "serve"
+      ];
+      requirements = [ pkgs.mypkgs.mox ];
+    };
+  };
+
+  containers = {
+    enable = true;
+    settings = {
+      container = {
+        name = "mox";
+        copyToRoot = [ pkgs.mypkgs.mox ];
+      };
+    };
+  };
+}
