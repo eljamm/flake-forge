9b42fd8f15e73149fe31c93ebb23ff6c967d62f8
eljamm <fedi.jamoussi@protonmail.ch>
2026-03-05 09:21:10 +0100

    feat: lazy system eval!!!

--- forge/modules/apps/nixos/default.nix
+++ forge/modules/apps/nixos/default.nix
@@ -4,6 +4,7 @@
 
   app,
   config,
+  pkgs,
   ...
 }:
 {
@@ -97,63 +98,73 @@
         '';
       };
 
+      eval = lib.mkOption {
+        internal = true;
+        readOnly = true;
+        type = lib.types.lazyAttrsOf lib.types.attrs;
+        description = "NixOS system evaluation.";
+      };
+
       build = lib.mkOption {
         internal = true;
         readOnly = true;
         type = lib.types.package;
-        default =
-          let
-            forwardPortsAttrs =
-              ports:
-              map (
-                port:
-                let
-                  portSplit = lib.splitString ":" port;
-                in
-                {
-                  from = "host";
-                  host.port = lib.toInt (lib.elemAt portSplit 0);
-                  guest.port = lib.toInt (lib.elemAt portSplit 1);
-                }
-              ) ports;
-
-            eval = inputs.nixpkgs.lib.nixosSystem {
-              system = "x86_64-linux";
-              modules = [
-                (
-                  { modulesPath, ... }:
-                  {
-                    imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
-                    virtualisation.graphics = false;
-                    virtualisation.cores = app.vm.config.cores;
-                    virtualisation.memorySize = app.vm.config.memorySize;
-                    virtualisation.diskSize = app.vm.config.diskSize;
-                    virtualisation.forwardPorts = forwardPortsAttrs app.vm.config.ports;
-                  }
-                )
-                {
-                  users.users.root.password = "root";
-                  services.openssh.settings.PermitRootLogin = lib.mkForce "yes";
-                  services.openssh.settings.PasswordAuthentication = lib.mkForce true;
-                  services.getty.autologinUser = "root";
-                  environment.systemPackages = app.vm.requirements;
-                  networking.hostName = app.vm.name;
-                  networking.useDHCP = lib.mkForce true;
-                  networking.firewall.enable = lib.mkForce false;
-                  system.stateVersion = "25.11";
-                  passthru = { }; # FIX: why is this required?
-                }
-                {
-                  # modular services
-                  system = { inherit (app) services; };
-                }
-                config.system
-              ];
-            };
-          in
-          eval.config.system.build.vm;
-        description = ""; # TODO:
+        default = config.vm.eval.config.system.build.vm;
+        description = "NixOS Virtual Machine.";
       };
     };
+  };
+
+  config = lib.mkIf config.enable {
+    vm.eval =
+      let
+        forwardPortsAttrs =
+          ports:
+          map (
+            port:
+            let
+              portSplit = lib.splitString ":" port;
+            in
+            {
+              from = "host";
+              host.port = lib.toInt (lib.elemAt portSplit 0);
+              guest.port = lib.toInt (lib.elemAt portSplit 1);
+            }
+          ) ports;
+
+        system = inputs.nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (
+              { modulesPath, ... }:
+              {
+                imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
+                virtualisation.graphics = false;
+                virtualisation.cores = app.vm.config.cores;
+                virtualisation.memorySize = app.vm.config.memorySize;
+                virtualisation.diskSize = app.vm.config.diskSize;
+                virtualisation.forwardPorts = forwardPortsAttrs app.vm.config.ports;
+              }
+            )
+            {
+              users.users.root.password = "root";
+              services.openssh.settings.PermitRootLogin = lib.mkForce "yes";
+              services.openssh.settings.PasswordAuthentication = lib.mkForce true;
+              services.getty.autologinUser = "root";
+              environment.systemPackages = app.vm.requirements;
+              networking.hostName = app.vm.name;
+              networking.useDHCP = lib.mkForce true;
+              networking.firewall.enable = lib.mkForce false;
+              system.stateVersion = "25.11";
+            }
+            {
+              # modular services
+              system = { inherit (app) services; };
+            }
+            config.system
+          ];
+        };
+      in
+      system;
   };
 }
