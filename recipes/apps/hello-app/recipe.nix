95f4cf181e2367271a62a359d5211264cdf34f18
eljamm <fedi.jamoussi@protonmail.ch>
2026-03-06 11:48:30 +0100

    hello: disable old attributes; refactor

--- recipes/apps/hello-app/recipe.nix
+++ recipes/apps/hello-app/recipe.nix
@@ -1,7 +1,7 @@
 {
   config,
-  lib,
   pkgs,
+  lib,
   ...
 }:
 
@@ -9,13 +9,6 @@
   name = "hello-app";
   version = "1.0.0";
   description = "Say hello in multiple languages.";
-
-  programs = {
-    enable = true;
-    requirements = [
-      pkgs.mypkgs.hello
-    ];
-  };
 
   services.hello = {
     imports = [ pkgs.mypkgs.hello.services.default ];
@@ -65,37 +58,46 @@
       };
     };
 
-  containers = {
-    enable = true;
-    images = [
-      {
-        name = "hello-english";
-        requirements = [ pkgs.mypkgs.hello ];
-        config.CMD = [
-          "hello"
-          "--greeting"
-          "Hello"
-        ];
-      }
-      {
-        name = "hello-italian";
-        requirements = [ pkgs.mypkgs.hello ];
-        config.CMD = [
-          "hello"
-          "--greeting"
-          "Ciao"
-        ];
-      }
-      {
-        name = "hello-spanish";
-        requirements = [ pkgs.mypkgs.hello ];
-        config.CMD = [
-          "hello"
-          "--greeting"
-          "Hola"
-        ];
-      }
-    ];
-    composeFile = ./compose.yaml;
-  };
+  # TODO: remove
+
+  # programs = {
+  #   enable = true;
+  #   requirements = [
+  #     pkgs.mypkgs.hello
+  #   ];
+  # };
+
+  # containers = {
+  #   enable = true;
+  #   images = [
+  #     {
+  #       name = "hello-english";
+  #       requirements = [ pkgs.mypkgs.hello ];
+  #       config.CMD = [
+  #         "hello"
+  #         "--greeting"
+  #         "Hello"
+  #       ];
+  #     }
+  #     {
+  #       name = "hello-italian";
+  #       requirements = [ pkgs.mypkgs.hello ];
+  #       config.CMD = [
+  #         "hello"
+  #         "--greeting"
+  #         "Ciao"
+  #       ];
+  #     }
+  #     {
+  #       name = "hello-spanish";
+  #       requirements = [ pkgs.mypkgs.hello ];
+  #       config.CMD = [
+  #         "hello"
+  #         "--greeting"
+  #         "Hola"
+  #       ];
+  #     }
+  #   ];
+  #   composeFile = ./compose.yaml;
+  # };
 }
