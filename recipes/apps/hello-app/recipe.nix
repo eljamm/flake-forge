30c3a7ffcc8f9cbf40cc5a3de7c4fe32d464def5
eljamm <fedi.jamoussi@protonmail.ch>
2026-03-06 11:20:32 +0100

    hello: build env for deps; share common settings

--- recipes/apps/hello-app/recipe.nix
+++ recipes/apps/hello-app/recipe.nix
@@ -21,35 +21,49 @@
     imports = [ pkgs.mypkgs.hello.services.default ];
   };
 
-  oci = {
-    hello-english = {
-      enable = true;
-      settings.container = {
-        copyToRoot = [ pkgs.mypkgs.hello ];
+  oci =
+    let
+      commonSettings = {
+        copyToRoot = [
+          (pkgs.buildEnv {
+            name = "runtime-bins";
+            paths = with pkgs; [
+              mypkgs.hello
+              coreutils
+              bash
+            ];
+            pathsToLink = [ "/bin" ];
+          })
+        ];
         imageConfig.WorkingDir = "/";
       };
+    in
+    {
+      hello-english = {
+        enable = true;
+        settings.container = commonSettings;
+        extraConfig.hello.hello.extraArgs = [
+          "--greeting"
+          "Hello"
+        ];
+      };
+      hello-italian = {
+        enable = true;
+        settings.container = commonSettings;
+        extraConfig.hello.hello.extraArgs = [
+          "--greeting"
+          "Ciao"
+        ];
+      };
+      hello-spanish = {
+        enable = true;
+        settings.container = commonSettings;
+        extraConfig.hello.hello.extraArgs = [
+          "--greeting"
+          "Hola"
+        ];
+      };
     };
-    hello-italian = {
-      enable = true;
-      settings.container = {
-        copyToRoot = [ pkgs.mypkgs.hello ];
-        imageConfig.WorkingDir = "/";
-      };
-      extraConfig = {
-        hello.hello.extraArgs = "--greeting Ciao";
-      };
-    };
-    hello-spanish = {
-      enable = true;
-      settings.container = {
-        copyToRoot = [ pkgs.mypkgs.hello ];
-        imageConfig.WorkingDir = "/";
-      };
-      extraConfig = {
-        hello.hello.extraArgs = "--greeting Hola";
-      };
-    };
-  };
 
   containers = {
     enable = true;
