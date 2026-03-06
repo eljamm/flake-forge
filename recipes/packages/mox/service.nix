bad94c62a567455852efb9e8047ed20a1b21dc09
eljamm <fedi.jamoussi@protonmail.ch>
2026-03-06 07:08:48 +0100

    mox: add user and group to main service

--- recipes/packages/mox/service.nix
+++ recipes/packages/mox/service.nix
@@ -66,7 +66,7 @@ in
         before = [ "mox.service" ];
         serviceConfig = {
           StateDirectory = "mox";
-          WorkingDirectory = "/var/lib/mox";
+          WorkingDirectory = "%S/mox";
           Type = "oneshot";
           RemainAfterExit = true;
           User = "mox";
@@ -84,8 +84,10 @@ in
       after = [ "mox-setup.service" ];
       requires = [ "mox-setup.service" ];
       serviceConfig = {
+        User = "mox";
+        Group = "mox";
         StateDirectory = "mox";
-        WorkingDirectory = "/var/lib/mox";
+        WorkingDirectory = "%S/mox";
         Restart = "always";
       };
     };
